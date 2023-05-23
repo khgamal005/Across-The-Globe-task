05800d4371784098c84efb644a38d10363c299c7
khgama005 <khgamal005@gmail.com>
2023-05-23 04:48:43 +0300

    updat42

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import reportWebVitals from './reportWebVitals';
